6159fe1e987de23f39785f633b2958c06fe33e2b
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-09 17:33:40 +0300

    Четвертый вид методов

---
+++ Example_004/Example_004.cs
@@ -0,0 +1,25 @@
+﻿//4-й вид методов
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        int Method4(int count, string text)
+        {
+            int i = 0;
+            string result = string.Empty;
+
+            while (i < count)
+            {
+                result = result + text;
+                i++;
+            }
+            return result;
+        }
+
+        string res = Method4(10, "qwerty");
+        Console.WriteLine(res);
+    }
+}
