a84e701aadc40a1e88e6253820379a80209ad8a5
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-09 15:01:22 +0300

    Два разных способа написания одного и того же метода

--- Example_002/Example_002.cs
+++ Example_002/Example_002.cs
@@ -1,22 +1,22 @@
 ﻿//2-й вид методов
 
-// Console.Clear();
+//  Console.Clear();
 
-// void Method2(string msg)
-// {
-//     Console.WriteLine(msg);
-// }
+//  void Method2(string msg)
+//  {
+//      Console.WriteLine(msg);
+//  }
 
-// Method2(msg: "Текст сообщения");
+//  Method2(msg: "Текст сообщения");
 
-// void Method21(string msg, int count)
-// {
-//     int i = 0;
-//     while (i < count)
+//  void Method21(string msg, int count)
+//  {
+//      int i = 0;
+//      while (i < count)
 //     {
-//         Console.WriteLine(msg);
-//         i++;
+//          Console.WriteLine(msg);
+//          i++;
 //     }
-// }
+//  }
 
-// Method21("Текст сообщения", 4);
+//  Method21("Текст сообщения", 4);
